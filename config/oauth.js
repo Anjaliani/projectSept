561cb7761b02a73c25b09ca9da9b727eb280fd2e
Anjaliani <singhanjali00021@gmail.com>
2017-09-14 16:50:45 +0530

    facebook passport

---
+++ config/oauth.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '438440776550040',
+		'clientSecret': '43b9ccc205bc088d0b904e886227099c',
+		'callbackURL': 'http://localhost:8080/oauth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+}
